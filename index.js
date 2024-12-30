edf9c3f84678c4652483ff5da44066a100e0f973
Prabhanjan Amanta <pravanjan2003p@gmail.com>
2024-12-31 01:54:09 +0530

    Fully update

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ document.querySelector(".imgMobileClose").addEventListener("click", () =>{
     document.querySelector(".mobileSlide").style.right = "-100%";
     
 })
+
+// const check = document.getElementById('check');
+// const PS = document.getElementById('particles-js');
+// let scriptAdded = false;
+// check.addEventListener('click', () => {
+//     if (!scriptAdded) {
+//     scriptAdded = true;
+//     PS.style.display = 'block';
+//     PS.style.backgroundColor = 'black';
+// } else {
+//     scriptAdded = false;
+//     PS.style.display = 'none';
+//   }
+// });
